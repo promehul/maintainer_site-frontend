55ed0e87d69e38169c7c25a0dce44b60898f58ba
Ayush Bansal <ayu023ban@gmail.com>
2020-09-17 15:01:18 +0530

    Added Condition on rendering medium connecting icon

--- src/components/blog/blog-page.js
+++ src/components/blog/blog-page.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react'
-import { Card, Container, Segment, Icon, Loader } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Card, Container, Segment, Icon, Loader } from "semantic-ui-react";
 
-import BlogDetail from './blog-post-card'
-import { urlApiBlog } from '../../urls'
-import { MEDIUM_URL } from '../../consts'
+import BlogDetail from "./blog-post-card";
+import { urlApiBlog } from "../../urls";
+import { MEDIUM_URL } from "../../consts";
 
-import common from '../../css/page-common-styles.css'
+import common from "../../css/page-common-styles.css";
 
 class Blog extends Component {
   componentDidMount() {
-    const URL = urlApiBlog()
-    this.props.requestBlogData(URL)
+    const URL = urlApiBlog();
+    this.props.requestBlogData(URL);
   }
 
   render() {
     if (this.props.apiBlogData.loaded) {
-      let MEDIUM_PUBLICATION = this.props.apiInfoData.footerData.mediumSlug
+      let MEDIUM_PUBLICATION = this.props.apiInfoData.footerData.mediumSlug;
+
       return (
         <Container styleName="common.margin">
           <Card.Group itemsPerRow={3} stackable doubling>
@@ -23,21 +24,23 @@ class Blog extends Component {
               <BlogDetail info={info} key={id} />
             ))}
           </Card.Group>
-          <Segment basic padded textAlign="center">
-            <Icon
-              name="medium"
-              size="large"
-              link={true}
-              onClick={() =>
-                window.open(`${MEDIUM_URL}${MEDIUM_PUBLICATION}`, '_blank')
-              }
-            />
-          </Segment>
+          {MEDIUM_PUBLICATION && MEDIUM_PUBLICATION !== "" ? (
+            <Segment basic padded textAlign="center">
+              <Icon
+                name="medium"
+                size="large"
+                link={true}
+                onClick={() =>
+                  window.open(`${MEDIUM_URL}${MEDIUM_PUBLICATION}`, "_blank")
+                }
+              />
+            </Segment>
+          ) : null}
         </Container>
-      )
+      );
     } else {
-      return <Loader active size="large" />
+      return <Loader active size="large" />;
     }
   }
 }
-export default Blog
+export default Blog;
