091e5a57b0a5ba7f389aa408d8da3f5e37514b7e
Ayush Bansal <ayu023ban@gmail.com>
2020-09-18 23:40:47 +0530

    Add Message for no blogs in the maintainer site

--- src/components/blog/blog-page.js
+++ src/components/blog/blog-page.js
@@ -1,46 +1,58 @@
-import React, { Component } from "react";
-import { Card, Container, Segment, Icon, Loader } from "semantic-ui-react";
+import React, { Component } from 'react'
+import { Card, Container, Segment, Icon, Loader } from 'semantic-ui-react'
 
-import BlogDetail from "./blog-post-card";
-import { urlApiBlog } from "../../urls";
-import { MEDIUM_URL } from "../../consts";
+import BlogDetail from './blog-post-card'
+import { urlApiBlog } from '../../urls'
+import { MEDIUM_URL } from '../../consts'
 
-import common from "../../css/page-common-styles.css";
+import common from '../../css/page-common-styles.css'
 
 class Blog extends Component {
   componentDidMount() {
-    const URL = urlApiBlog();
-    this.props.requestBlogData(URL);
+    const URL = urlApiBlog()
+    this.props.requestBlogData(URL)
   }
 
   render() {
     if (this.props.apiBlogData.loaded) {
-      let MEDIUM_PUBLICATION = this.props.apiInfoData.footerData.mediumSlug;
+      if (this.props.apiBlogData.data && this.props.apiBlogData.data.length) {
+        let MEDIUM_PUBLICATION = this.props.apiInfoData.footerData.mediumSlug
 
-      return (
-        <Container styleName="common.margin">
-          <Card.Group itemsPerRow={3} stackable doubling>
-            {this.props.apiBlogData.data.map((info, id) => (
-              <BlogDetail info={info} key={id} />
-            ))}
-          </Card.Group>
-          {MEDIUM_PUBLICATION && MEDIUM_PUBLICATION !== "" ? (
+        return (
+          <Container styleName="common.margin">
+            <Card.Group itemsPerRow={3} stackable doubling>
+              {this.props.apiBlogData.data.map((info, id) => (
+                <BlogDetail info={info} key={id} />
+              ))}
+            </Card.Group>
             <Segment basic padded textAlign="center">
-              <Icon
-                name="medium"
-                size="large"
-                link={true}
-                onClick={() =>
-                  window.open(`${MEDIUM_URL}${MEDIUM_PUBLICATION}`, "_blank")
-                }
-              />
+              {MEDIUM_PUBLICATION && MEDIUM_PUBLICATION !== '' ? (
+                <Icon
+                  name="medium"
+                  size="large"
+                  link={true}
+                  onClick={() =>
+                    window.open(`${MEDIUM_URL}${MEDIUM_PUBLICATION}`, '_blank')
+                  }
+                />
+              ) : (
+                `This group doesn't have any blogs as of now. Check back later.`
+              )}
             </Segment>
-          ) : null}
-        </Container>
-      );
+          </Container>
+        )
+      } else {
+        return (
+          <Container styleName="common.margin">
+            <Segment basic padded textAlign="center">
+              {`This group doesn't have any blogs as of now. Check back later.`}
+            </Segment>
+          </Container>
+        )
+      }
     } else {
-      return <Loader active size="large" />;
+      return <Loader active size="large" />
     }
   }
 }
-export default Blog;
+export default Blog
